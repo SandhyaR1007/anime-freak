e9d0659b545f6dc836cdfa210d11d9e8eff7e71e
Sandhya Rajwanshi <grajwanshi1008@gmail.com>
2022-08-30 17:41:02 +0000

    Display list and category filter for anime data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./styles.css";
+import { useState } from "react";
+import { animes } from "./animes";
 
 export default function App() {
+  const [type, setType] = useState("Movies");
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>
+        <span>🍥</span> animeBuzz
+      </h1>
+      <p>
+        Here are some of my favorite anime. Select a category to get started
+      </p>
+      <div>
+        {Object.keys(animes).map((i) => (
+          <span id="category" key={i} onClick={() => setType(i)}>
+            {i}
+          </span>
+        ))}
+      </div>
+      <div className="divider"></div>
+      <div>
+        {animes[type].map(({ name, japanese, rating }, index) => {
+          return (
+            <div className="description" key={index}>
+              <div className="title">
+                {name} ({japanese})
+              </div>
+              <div className="info">{rating}/10</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
